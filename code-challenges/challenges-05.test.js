05c1ba559268eba99fb11732b3bd8cb451b25345
Daniel Dills <dills253@gmail.com>
2021-04-07 11:27:05 -0700

    completed two more code challenges

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -48,7 +48,9 @@ Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
 const countNumberOfElements = (arr) => {
-  // Solution code here...
+  return arr.reduce( (answerSoFar, current) => {
+    return answerSoFar + 1;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -108,7 +110,10 @@ let starWarsData = [{
 }];
 
 const returnNames = (arr) => {
-  // Solution code here...
+  return arr.reduce ( (answerSoFar, current) => {
+    answerSoFar.push(current.name);
+    return answerSoFar;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
